54b5e8a0da3c4452d5e9271c2c80539522a64d72
Waheed Uddin Ahmed <waheed.ahmed@northsouth.edu>
2019-06-20 21:39:43 +0600

    made some minor changes to the testing(adding new inputs)

--- Project 1/src/application/MainController.java
+++ Project 1/src/application/MainController.java
@@ -23,9 +23,13 @@ public class MainController implements EventHandler<ActionEvent>{
 @FXML
 public TextField fname;
 @FXML
+public TextField lname;
+@FXML
+public TextField mail;
+@FXML
 public Button Generate;
 @FXML
-String n;
+String n,m;
 public void click(ActionEvent args) {
 //	 n = fname.getText();
 	Generate.setOnAction(this);
@@ -36,7 +40,8 @@ public void handle(ActionEvent args) {
 	if(args.getSource()==Generate) {
 		
 		String n = fname.getText();
-		
+		String m = lname.getText();
+		String g = mail.getText();
 		String filename = "project.html";
 		
 		try {
@@ -50,14 +55,23 @@ public void handle(ActionEvent args) {
 					"    <script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\"></script>\r\n" + 
 					"    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js\"></script>\r\n" + 
 					"    <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\"></script>\r\n" + 
-					"\r\n" +  
+					"\r\n" + 
 					"    <title></title>\r\n" + 
 					"  </head>\r\n" + 
-					"  <body><h1>Hello World! My name is " + n + " </h1>" +
-					 "</body>\r\n" + 
-					"</html>");
+					"\r\n" + 
+					"  <body>\r\n" + 
+					"    <div class=\"container\">\r\n" + 
+					"      <h2 id=\"fname\">First Name: " + m + "</h2>\r\n" + 
+					"      <h2 id=\"lname\">Last Name: " + n + "</h2>\r\n" + 
+					"      <h2 id=\"mail\">Email:" + g + " </h2>\r\n" + 
+					"\r\n" + 
+					"    </div>\r\n" + 
+					"\r\n" + 
+					"  </body>\r\n" + 
+					"</html>\r\n" + 
+					"");
 			outputStream.close();
-			System.out.println("Printed!");
+			System.out.println("Printed!" );
 			
 		}catch(FileNotFoundException e) {
 			e.printStackTrace();
