4a5597c6d3d715db8ff550dfc151422511d76b35
Waheed Uddin Ahmed <waheed.ahmed@northsouth.edu>
2019-06-24 20:01:52 +0600

    made changes to the cv html template

--- Project 1/src/application/Main.java
+++ Project 1/src/application/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
 			
 			Parent root = FXMLLoader.load(getClass().getResource("Design.fxml"));
 //			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,1280,720);
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
--- Project 1/src/application/MainController.java
+++ Project 1/src/application/MainController.java
@@ -40,7 +40,7 @@ public void handle(ActionEvent args) {
 	if(args.getSource()==Generate) {
 		
 		String m = fname.getText();
-		String n = lname.getText();
+		
 		String g = mail.getText();
 		String filename = "project.html";
 		
@@ -56,11 +56,51 @@ public void handle(ActionEvent args) {
 					"    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js\"></script>\r\n" + 
 					"    <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\"></script>\r\n" + 
 					"    <link rel=\"stylesheet\" href=\"project.css\">\r\n" + 
-					"    <title>CV</title>\r\n" + 
+					"    <title>RESUME | CV</title>\r\n" + 
 					"  </head>\r\n" + 
 					"\r\n" + 
 					"<style media=\"screen\">\r\n" + 
+					"  footer{\r\n" + 
+					"          margin-bottom: 0px;\r\n" + 
+					"          margin-top: 150px;\r\n" + 
+					"          height:  300px;\r\n" + 
+					"          background-color: #005073;\r\n" + 
 					"\r\n" + 
+					"\r\n" + 
+					"          color: white;\r\n" + 
+					"        }\r\n" + 
+					"        .row{\r\n" + 
+					"          display: flex;\r\n" + 
+					"          flex-direction: row;\r\n" + 
+					"\r\n" + 
+					"        }\r\n" + 
+					"        .col-md-4{\r\n" + 
+					"          text-align: center;\r\n" + 
+					"          margin-top: 30px;\r\n" + 
+					"          letter-spacing: 4px;\r\n" + 
+					"        }\r\n" + 
+					"        .col-md-4 hr{\r\n" + 
+					"          height:1px;\r\n" + 
+					"        }\r\n" + 
+					"        .col-md-4 .lead {\r\n" + 
+					"          font-size: 25px;\r\n" + 
+					"          font-weight: 200;\r\n" + 
+					"        }\r\n" + 
+					"\r\n" + 
+					"\r\n" + 
+					"        @media only screen and (max-width: 768px) {\r\n" + 
+					"          footer{\r\n" + 
+					"            height: 550px;\r\n" + 
+					"\r\n" + 
+					"\r\n" + 
+					"          }\r\n" + 
+					"        }\r\n" + 
+					"\r\n" + 
+					"        @media only screen and (max-width: 480px) {\r\n" + 
+					"          footer{\r\n" + 
+					"            background-color: black !important;\r\n" + 
+					"          }\r\n" + 
+					"        }\r\n" + 
 					"</style>\r\n" + 
 					"\r\n" + 
 					"  <body>\r\n" + 
@@ -70,14 +110,29 @@ public void handle(ActionEvent args) {
 					"      <div class=\"container-fluid mt-3 background\"></div>\r\n" + 
 					"      <div class=\"container\">\r\n" + 
 					"       <div id=\"pic\"></div>\r\n" + 
-					"       <h3 id=\"Name\" class=\"lead\"><span>" + m + ' ' + n + "</span></h3>\r\n" + 
+					"       <h3 id=\"Name\" class=\"lead\"><span>Waheed Uddin  Ahmed</span></h3>\r\n" + 
 					"       <div id = \"info\" >\r\n" + 
 					"       <h5>MY INFO</h5>\r\n" + 
 					"       <hr color=\"silver\"  width=\"80px\">\r\n" + 
-					"       <h3 class=\"ml-5\">Full Name:  <span>"+m+n+"</span></h3>\r\n" + 
-					"       <h3 class=\"ml-5\">Email:    <span>"+g+"</span></h3>\r\n" + 
+					"       <h3 class=\"ml-5\">Full Name:  <span>Waheed Uddin Ahmed</span></h3>\r\n" + 
+					"       <h3 class=\"ml-5\">Email:    <span>dmc4719@gmail.com</span></h3>\r\n" + 
+					"       <h3 class=\"ml-5\">Github:    <span></span></h3>\r\n" + 
+					"       <h3 class=\"ml-5\">LinkedIn:    <span></span></h3>\r\n" + 
 					"       </div>\r\n" + 
-					"\r\n" + 
+					"       <div id = \"info\" >\r\n" + 
+					"       <h5>EDUCATION</h5>\r\n" + 
+					"       <hr color=\"silver\"  width=\"115px\">\r\n" + 
+					"       <h3 class=\"ml-5\">Undergraduate<span></span></h3>\r\n" + 
+					"       <h3 class=\"ml-5\">Studied Software Engineering at North South University<span></span></h3>\r\n" + 
+					"       <h3 class=\"ml-5\">CGPA: <span></span></h3>\r\n" + 
+					"       </div>\r\n" + 
+					"       <div id = \"info\" >\r\n" + 
+					"       <h5>SKILLS</h5>\r\n" + 
+					"       <hr color=\"silver\"  width=\"45px\">\r\n" + 
+					"       <h3 class=\"ml-5\">Front-end, UX,UI Developer<span></span></h3>\r\n" + 
+					"       <h3 class=\"ml-5\">Back-end Developer<span></span></h3>\r\n" + 
+					"       <h3 class=\"ml-5\">Fluency with various programming languages and frameworks such Python, R, Java, C++, Bootstrap, HTML, CSS, JS, Django, Laravel, etc.<span></span></h3>\r\n" + 
+					"       </div>\r\n" + 
 					"\r\n" + 
 					"\r\n" + 
 					"\r\n" + 
@@ -91,7 +146,36 @@ public void handle(ActionEvent args) {
 					"\r\n" + 
 					"\r\n" + 
 					"\r\n" + 
+					"    <footer>\r\n" + 
 					"\r\n" + 
+					"      <div class=\"row\">\r\n" + 
+					"        <div class=\"col-md-4 \">\r\n" + 
+					"          <h3 class = \"lead\">EDUCATION</h3>\r\n" + 
+					"          <hr color=\"silver\"  width=\"80px\">\r\n" + 
+					"          <h4>Github</h4>\r\n" + 
+					"          <h4>Piazza</h4>\r\n" + 
+					"\r\n" + 
+					"        </div>\r\n" + 
+					"        <div class=\"col-md-4\">\r\n" + 
+					"          <h3 class = \"lead\">SOCIAL</h3>\r\n" + 
+					"          <hr color=\"silver\"  width=\"80px\">\r\n" + 
+					"          <h4>Github</h4>\r\n" + 
+					"          <h4>Piazza</h4>\r\n" + 
+					"\r\n" + 
+					"        </div>\r\n" + 
+					"        <div class=\"col-md-4\">\r\n" + 
+					"          <h3 class = \"lead\">RANDOM</h3>\r\n" + 
+					"          <hr color=\"silver\"  width=\"80px\">\r\n" + 
+					"          <h4>Github</h4>\r\n" + 
+					"          <h4>Piazza</h4>\r\n" + 
+					"\r\n" + 
+					"        </div>\r\n" + 
+					"\r\n" + 
+					"        </div>\r\n" + 
+					"\r\n" + 
+					"\r\n" + 
+					"\r\n" + 
+					"    </footer>\r\n" + 
 					"\r\n" + 
 					"  </body>\r\n" + 
 					"</html>\r\n" + 
