81025c70aaea52d80979682024c53bc71b5d88dc
Waheed Uddin Ahmed <waheed.ahmed@northsouth.edu>
2019-07-08 00:29:10 +0600

    added two vboxes to show the new dynamically generated skills fields

--- Project 1/src/application/MainController.java
+++ Project 1/src/application/MainController.java
@@ -51,7 +51,10 @@ public VBox dood;
 @FXML
 public HBox next[] = new HBox[20];
 @FXML
-public Label sk[] = new Label[20];
+public VBox dyvbox1;
+@FXML
+public VBox dyvbox2;;
+
 @FXML
 public TextField textField[] = new TextField[20];
 public static String tex[] = new String[20];
@@ -97,149 +100,14 @@ public void handle(ActionEvent args) {
 			Boolean mob_check = mobile_check(mob);
 			Boolean bool = isempty(m,g,addr,mob);
 			Boolean dy = dycheck(tex,last,textField);
-			    if(bool.equals(false) && g_check.equals(true) && mob_check.equals(true) && dy.equals(true)) {
+			    if(bool.equals(false) && g_check.equals(true) && mob_check.equals(true) ) {
 			
 			
 				String filename = "CV of " + m + ".html";
 		
 				try {
 					PrintWriter outputStream = new PrintWriter(filename);
-//					outputStream.println("<!DOCTYPE html>\r\n" + 
-//					"<html lang=\"en\" dir=\"ltr\">\r\n" + 
-//					"  <head>\r\n" + 
-//					"    <meta charset=\"utf-8\">\r\n" + 
-//					"    <link href=\"https://fonts.googleapis.com/css?family=Nunito:200,600\" rel=\"stylesheet\">\r\n" + 
-//					"    <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\">\r\n" + 
-//					"    <script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\"></script>\r\n" + 
-//					"    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js\"></script>\r\n" + 
-//					"    <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\"></script>\r\n" + 
-//					"    <link rel=\"stylesheet\" href=\"project.css\">\r\n" + 
-//					"    <title>RESUME | CV</title>\r\n" + 
-//					"  </head>\r\n" + 
-//					"\r\n" + 
-//					"<style media=\"screen\">\r\n" + 
-//					"  footer{\r\n" + 
-//					"          margin-bottom: 0px;\r\n" + 
-//					"          margin-top: 150px;\r\n" + 
-//					"          height:  300px;\r\n" + 
-//					"          background-color: #005073;\r\n" + 
-//					"\r\n" + 
-//					"\r\n" + 
-//					"          color: white;\r\n" + 
-//					"        }\r\n" + 
-//					"        .row{\r\n" + 
-//					"          display: flex;\r\n" + 
-//					"          flex-direction: row;\r\n" + 
-//					"\r\n" + 
-//					"        }\r\n" + 
-//					"        .col-md-4{\r\n" + 
-//					"          text-align: center;\r\n" + 
-//					"          margin-top: 30px;\r\n" + 
-//					"          letter-spacing: 4px;\r\n" + 
-//					"        }\r\n" + 
-//					"        .col-md-4 hr{\r\n" + 
-//					"          height:1px;\r\n" + 
-//					"        }\r\n" + 
-//					"        .col-md-4 .lead {\r\n" + 
-//					"          font-size: 25px;\r\n" + 
-//					"          font-weight: 200;\r\n" + 
-//					"        }\r\n" + 
-//					"\r\n" + 
-//					"\r\n" + 
-//					"        @media only screen and (max-width: 768px) {\r\n" + 
-//					"          footer{\r\n" + 
-//					"            height: 550px;\r\n" + 
-//					"\r\n" + 
-//					"\r\n" + 
-//					"          }\r\n" + 
-//					"        }\r\n" + 
-//					"\r\n" + 
-//					"        @media only screen and (max-width: 480px) {\r\n" + 
-//					"          footer{\r\n" + 
-//					"            background-color: black !important;\r\n" + 
-//					"          }\r\n" + 
-//					"        }\r\n" + 
-//					"</style>\r\n" + 
-//					"\r\n" + 
-//					"  <body>\r\n" + 
-//					"    <div class=\"wrapper \">\r\n" + 
-//					"      <h1 class=\"mt-3 lead heading\"align= center >RESUME <span class =\"i\">|</span> CV</h1>\r\n" + 
-//					"      <hr color=\"silver\"  width=\"150px\">\r\n" + 
-//					"      <div class=\"container-fluid mt-3 background\"></div>\r\n" + 
-//					"      <div class=\"container\">\r\n" + 
-//					"       <div id=\"pic\"></div>\r\n" + 
-//
-//					"       <h3 id=\"Name\" class=\"lead\"><span>Waheed Uddin  Ahmed</span></h3>\r\n" + 
-//					"       <div id = \"info\" >\r\n" + 
-//					"       <h5>MY INFO</h5>\r\n" + 
-//					"       <hr color=\"silver\"  width=\"80px\">\r\n" + 
-//					"       <h3 class=\"ml-5\">Full Name:  <span>Waheed Uddin Ahmed</span></h3>\r\n" + 
-//					"       <h3 class=\"ml-5\">Email:    <span>dmc4719@gmail.com</span></h3>\r\n" + 
-//					"       <h3 class=\"ml-5\">Github:    <span></span></h3>\r\n" + 
-//					"       <h3 class=\"ml-5\">LinkedIn:    <span></span></h3>\r\n" + 
-//					"       </div>\r\n" + 
-//					"       <div id = \"info\" >\r\n" + 
-//					"       <h5>EDUCATION</h5>\r\n" + 
-//					"       <hr color=\"silver\"  width=\"115px\">\r\n" + 
-//					"       <h3 class=\"ml-5\">Undergraduate<span></span></h3>\r\n" + 
-//					"       <h3 class=\"ml-5\">Studied Software Engineering at North South University<span></span></h3>\r\n" + 
-//					"       <h3 class=\"ml-5\">CGPA: <span></span></h3>\r\n" + 
-//					"       </div>\r\n" + 
-//					"       <div id = \"info\" >\r\n" + 
-//					"       <h5>SKILLS</h5>\r\n" + 
-//					"       <hr color=\"silver\"  width=\"45px\">\r\n" + 
-//					"       <h3 class=\"ml-5\">Front-end, UX,UI Developer<span></span></h3>\r\n" + 
-//					"       <h3 class=\"ml-5\">Back-end Developer<span></span></h3>\r\n" + 
-//					"       <h3 class=\"ml-5\">Fluency with various programming languages and frameworks such Python, R, Java, C++, Bootstrap, HTML, CSS, JS, Django, Laravel, etc.<span></span></h3>\r\n" + 
-//
-//					
-//					"\r\n" + 
-//					"\r\n" + 
-//					"\r\n" + 
-//					"\r\n" + 
-//					"       </div>\r\n" + 
-//					"    </div>\r\n" + 
-//					"\r\n" + 
-//					"\r\n" + 
-//					"\r\n" + 
-//					"\r\n" + 
-//					"\r\n" + 
-//					"\r\n" + 
-//					"\r\n" + 
-//					"    <footer>\r\n" + 
-//					"\r\n" + 
-//					"      <div class=\"row\">\r\n" + 
-//					"        <div class=\"col-md-4 \">\r\n" + 
-//					"          <h3 class = \"lead\">EDUCATION</h3>\r\n" + 
-//					"          <hr color=\"silver\"  width=\"80px\">\r\n" + 
-//					"          <h4>Github</h4>\r\n" + 
-//					"          <h4>Piazza</h4>\r\n" + 
-//					"\r\n" + 
-//					"        </div>\r\n" + 
-//					"        <div class=\"col-md-4\">\r\n" + 
-//					"          <h3 class = \"lead\">SOCIAL</h3>\r\n" + 
-//					"          <hr color=\"silver\"  width=\"80px\">\r\n" + 
-//					"          <h4>Github</h4>\r\n" + 
-//					"          <h4>Piazza</h4>\r\n" + 
-//					"\r\n" + 
-//					"        </div>\r\n" + 
-//					"        <div class=\"col-md-4\">\r\n" + 
-//					"          <h3 class = \"lead\">RANDOM</h3>\r\n" + 
-//					"          <hr color=\"silver\"  width=\"80px\">\r\n" + 
-//					"          <h4>Github</h4>\r\n" + 
-//					"          <h4>Piazza</h4>\r\n" + 
-//					"\r\n" + 
-//					"        </div>\r\n" + 
-//					"\r\n" + 
-//					"        </div>\r\n" + 
-//					"\r\n" + 
-//					"\r\n" + 
-//					"\r\n" + 
-//					"    </footer>\r\n" + 
-//					"\r\n" + 
-//					"  </body>\r\n" + 
-//					"</html>\r\n" + 
-//					"");
+
 					
 					String fp = ("<!DOCTYPE html>\r\n" + 
 							"<html lang=\"en\" dir=\"ltr\">\r\n" + 
@@ -313,27 +181,26 @@ public void handle(ActionEvent args) {
 							"       <h3 class=\"ml-5\">Full Name:  <span>"+ m + "</span></h3>\r\n" + 
 							"       <h3 class=\"ml-5\">Email:    <span>"+ g +"</span></h3>\r\n" + 
 							"       <h3 class=\"ml-5\">Github:    <span></span></h3>\r\n" + 
-							"       <h3 class=\"ml-5\">LinkedIn:    <span></span></h3>\r\n" );
-					
-					
-					String mid = "";
-					String mid1;
-					for(int w = 0;w<last;w++) {
-						mid1 =  "<h3 class=\"ml-5\">Skill :<span>"+ textField[w].getText() + "</span></h3>\r\n"  ;
-						
-						mid = mid + mid1;
-					}
-							String lp ="       </div><div id = \"info\" >\r\n" + 
-									"       <h5>EDUCATION</h5>\r\n" + 
-									"       <hr color=\"silver\"  width=\"115px\">\r\n" + 
-									"       <h3 class=\"ml-5\">Undergraduate<span></span></h3>\r\n" + 
-									"       <h3 class=\"ml-5\">Studied Software Engineering at North South University<span></span></h3>\r\n" + 
-									"       <h3 class=\"ml-5\">CGPA: <span></span></h3>\r\n" + 
-									"       </div>\r\n" + 
-									"       <div id = \"info\" >\r\n" + 
-									"       <h5>SKILLS</h5>\r\n" + 
-									"       <hr color=\"silver\"  width=\"45px\">\r\n" + 
-									"       <h3 class=\"ml-5\">Front-end, UX,UI Developer<span></span></h3>\r\n" + 
+							"       <h3 class=\"ml-5\">LinkedIn:    <span></span></h3>\r\n" +
+							"       </div><div id = \"info\" >\r\n" + 
+							"       <h5>EDUCATION</h5>\r\n" + 
+							"       <hr color=\"silver\"  width=\"115px\">\r\n" + 
+							"       <h3 class=\"ml-5\">Undergraduate<span></span></h3>\r\n" + 
+							"       <h3 class=\"ml-5\">Studied Software Engineering at North South University<span></span></h3>\r\n" + 
+							"       <h3 class=\"ml-5\">CGPA: <span></span></h3>\r\n" + 
+							"       </div>\r\n" + 
+							"       <div id = \"info\" >\r\n" +
+							"       <h5>SKILLS</h5>\r\n" + 
+							"       <hr color=\"silver\"  width=\"45px\">\r\n" ) ;
+									
+									String mid = "";
+									String mid1;
+									for(int w = 0;w<last;w++) {
+										mid1 =  "<h3 class=\"ml-5\">"+ textField[w].getText() + "</h3>\r\n"  ;
+										
+										mid = mid + mid1;
+									}			
+						String lp=	"       <h3 class=\"ml-5\">Front-end, UX,UI Developer<span></span></h3>\r\n" + 
 									"       <h3 class=\"ml-5\">Back-end Developer<span></span></h3>\r\n" + 
 									"       <h3 class=\"ml-5\">Fluency with various programming languages and frameworks such Python, R, Java, C++, Bootstrap, HTML, CSS, JS, Django, Laravel, etc.<span></span></h3>\r\n" + 
 				
@@ -410,32 +277,44 @@ public void handle(ActionEvent args) {
 	}
 	
 	if(args.getSource()==newskill) {
-		if(i<3) {
-	
-     next[i]=new HBox();
-     next[i].setPrefWidth(309);
-     next[i].setStyle("-fx-margin:20px 0px 0px 0px;");
-     next[i].getStyleClass().add("dy-hbox");
+		if(i<8) {
+	if(i==0 || i%2==0) {
+		
+		
+		  textField[i] = new TextField();
+	      
+	      textField[i].getStyleClass().add("dy-text");
+	      
+	      textField[i].setPromptText("Add a skill");
+	      dyvbox1.getChildren().add(textField[i]);
+	      dyvbox1.setSpacing(20);
+	}
+	else {
+//     next[i]=new HBox();
+//     next[i].setPrefWidth(309);
+//     next[i].setStyle("-fx-margin:20px 0px 0px 0px;");
+//     next[i].getStyleClass().add("dy-hbox");
      
     
-     sk[i]=new Label();
-     sk[i].setText("Skill# "+j);
-     sk[i].setPrefWidth(77);
-     sk[i].getStyleClass().add("dy-label");
+//     sk[i]=new Label();
+//     sk[i].setText("Skill# "+j);
+//     sk[i].setPrefWidth(77);
+//     sk[i].getStyleClass().add("dy-label");
       textField[i] = new TextField();
       
       textField[i].getStyleClass().add("dy-text");
       
       textField[i].setPromptText("Add a skill");
-      next[i].getChildren().addAll(sk[i],textField[i]);
+      dyvbox2.getChildren().addAll(textField[i]);
       
-      dood.getChildren().add(next[i]);
-      dood.setSpacing(5);
-      dood.setStyle("-fx-padding:0px 0px 20px 0px");
-      i = i+1;
-      last = i;
-      j=j+1;
-  
+//      dood.getChildren().add(next[i]);
+      dyvbox2.setSpacing(20);
+//      dood.setStyle("-fx-padding:0px 0px 20px 0px");
+     
+	}
+	 i = i+1;
+     last = i;
+     j=j+1;
 		}
 		else {}
 	
