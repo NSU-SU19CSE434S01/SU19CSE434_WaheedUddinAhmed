786f94e56f074c90af3d1bc153ecdf392f57ab83
Waheed Uddin Ahmed <waheed.ahmed@northsouth.edu>
2019-07-21 12:04:14 +0600

    added the first test code and first block for name validation

--- Project 1/src/application/name_validationtest.java
+++ Project 1/src/application/name_validationtest.java
@@ -10,16 +10,24 @@ public class name_validationtest  {
 	
 	
 	
+	/*
+	 *Case1: Null input check
+	 *Case2: Empty String input check
+	 *Case3: Special Characters input check
+	 *Case4: Number input check
+	 *Case5: Alphabets check
+	*/
 	
 	
-	
-	
-	
-	
-	
+	@Test			//Case1
+	public void testCase1() {
+		Boolean value = mcont.name_check(null);
+		assertFalse(value);
+	}			//Checking whether the output is false using assertFalse as 'null' is not a valid name input.
 	
 	
 
+	
 	
 	
 
