233d21dd9de3f10f08f3c60bf45240e8648a7599
Waheed Uddin Ahmed <waheed.ahmed@northsouth.edu>
2019-08-10 22:36:49 +0600

    changed from sample site testing to PHP TRAVELS site

--- Project 2/src/Selenium_Tests/HelloWorld.java
+++ Project 2/src/Selenium_Tests/HelloWorld.java
@@ -1,22 +1,40 @@
 package Selenium_Tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HelloWorld {
+	
+	
 
 	public static void main(String[] args) throws InterruptedException{
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\dmc47\\Downloads\\Compressed\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.gmail.com/");
-		driver.manage().window().maximize();
+	
+		check_login();
+		
+		
+	}
 
+	private static boolean check_login() throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.phptravels.com/");
+		driver.manage().window().maximize();
 		Thread.sleep(2000);
-		driver.findElement(By.id("identifierId")).sendKeys("www.gmail.com");
-		Thread.sleep(2000);
-		driver.findElement(By.className("RveJvd")).click();
+		driver.findElement(By.className("login")).click();
+		
+		Thread.sleep(1000);
+//		driver.findElement(By.id("inputEmail")).sendKeys("waheed@gmail.com");
+		driver.findElement(By.id("inputPassword")).click();
+		driver.findElement(By.id("inputPassword")).sendKeys("starboi");
+		Thread.sleep(1000);
+		driver.findElement(By.id("recaptcha-anchor")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.id("login")).click();
 		String at = driver.getTitle();
-		String et = "gmail";
+		String et = "Client Area - PHP TRAVELS";
 		driver.close();
 		if(at.equalsIgnoreCase(et)) {
 			System.out.println("Test Successful");
@@ -24,6 +42,8 @@ public class HelloWorld {
 		else {
 			System.out.println("Test UnSuccessful");
 		}
+		
+		return false;
 		
 	}
 	
