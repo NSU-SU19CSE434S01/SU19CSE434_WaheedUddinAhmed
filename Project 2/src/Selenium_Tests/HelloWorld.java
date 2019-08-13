9747538768a056867368d4a5df8514d489757d9a
Waheed Uddin Ahmed <waheed.ahmed@northsouth.edu>
2019-08-13 22:24:23 +0600

    changed website to test and also added TestNG library to project

--- Project 2/src/Selenium_Tests/HelloWorld.java
+++ Project 2/src/Selenium_Tests/HelloWorld.java
@@ -1,8 +1,14 @@
 package Selenium_Tests;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HelloWorld {
 	
@@ -10,42 +16,55 @@ public class HelloWorld {
 
 	public static void main(String[] args) throws InterruptedException{
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\dmc47\\Downloads\\Compressed\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\dmc47\\Downloads\\Compressed\\chromedriver_win32\\geckodriver.exe");
+
+		//		WebDriver driver = new FirefoxDriver();
+		
 		WebDriver driver = new ChromeDriver();
-	
-		check_login();
+		
+
+		driver.get("http://automationpractice.com/index.php");
+		driver.manage().window().maximize();
+		
+		WebElement email;
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		email = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a")));
+		driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a")).click();
+		
+		driver.findElement(By.xpath("//*[@id=\"email_create\"]")).click();
+		
+		driver.findElement(By.xpath("//*[@id=\"email_create\"]")).sendKeys("Waheed.Ahmed@northsouth.edu");
+		
+		
+		
+//	
+//		Thread.sleep(1000);
+//		driver.findElement(By.xpath("//*[@id=\"inputPassword\"]")).click();
+//		driver.findElement(By.xpath("//*[@id=\"inputPassword\"]")).sendKeys("starboi");
+//		Thread.sleep(1000);
+//		driver.findElement(By.id("recaptcha-anchor")).click();
+//		Thread.sleep(1000);
+//		driver.findElement(By.id("login")).click();
+//		String at = driver.getTitle();
+//		String et = "Client Area - PHP TRAVELS";
+//		driver.close();
+//		if(at.equalsIgnoreCase(et)) {
+//			System.out.println("Test Successful");
+//		}
+//		else {
+//			System.out.println("Test UnSuccessful");
+//		}
+		
+		
+		
+		
+		
+//		check_login();
 		
 		
 	}
 
-	private static boolean check_login() throws InterruptedException {
-		
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.phptravels.com/");
-		driver.manage().window().maximize();
-		Thread.sleep(2000);
-		driver.findElement(By.className("login")).click();
-		
-		Thread.sleep(1000);
-//		driver.findElement(By.id("inputEmail")).sendKeys("waheed@gmail.com");
-		driver.findElement(By.id("inputPassword")).click();
-		driver.findElement(By.id("inputPassword")).sendKeys("starboi");
-		Thread.sleep(1000);
-		driver.findElement(By.id("recaptcha-anchor")).click();
-		Thread.sleep(1000);
-		driver.findElement(By.id("login")).click();
-		String at = driver.getTitle();
-		String et = "Client Area - PHP TRAVELS";
-		driver.close();
-		if(at.equalsIgnoreCase(et)) {
-			System.out.println("Test Successful");
-		}
-		else {
-			System.out.println("Test UnSuccessful");
-		}
-		
-		return false;
-		
-	}
+
 	
 	
 }
