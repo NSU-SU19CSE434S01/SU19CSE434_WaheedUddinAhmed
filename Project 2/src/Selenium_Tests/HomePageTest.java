97075b0c843f34c1712a154c4d24335d9161559f
Waheed Uddin Ahmed <waheed.ahmed@northsouth.edu>
2019-08-29 22:09:06 +0600

    added another function make_a_purchase

--- Project 2/src/Selenium_Tests/HomePageTest.java
+++ Project 2/src/Selenium_Tests/HomePageTest.java
@@ -38,52 +38,18 @@ public class HomePageTest {
 		
 	}
 	
-	@Test(priority=1)
-	public void is_Sign_Out_Working() {
-		if(driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[2]/a")).isDisplayed()) {
-		  driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[2]/a")).click();
-		  }
-		Boolean flag = driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a")).isDisplayed();
-		Assert.assertTrue(flag);
-		  
-	}  
-	
-	
-	@Test(priority=2)
-	public void click_On_Personal_Info() {
-		
-		  driver.findElement(By.xpath("//*[@id=\"center_column\"]/div/div[1]/ul/li[4]/a/span")).click();
-		  String Text= "YOUR PERSONAL INFORMATION";
-		  if(driver.findElement(By.xpath("//*[@id=\"center_column\"]/div/h1")).isDisplayed()) {
-			  String SegmentTitle = driver.findElement(By.xpath("//*[@id=\"center_column\"]/div/h1")).getText();
-			  Assert.assertEquals(SegmentTitle,Text);
-		  }
-		  
-	}
-	
-	
-	
-	@Test(priority=3)
-	public void Add_to_cart() {
-		
-		  driver.findElement(By.xpath("//*[@id=\"block_top_menu\"]/ul/li[2]/a")).click();
-		WebElement e_to_hover_over=  driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[1]/div/a[1]/img"));
-		Hover(driver, e_to_hover_over);
-		driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/div[2]/a[1]/span")).click();
-		
-		driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/a/span")).click();
-		
-		
-		String extracted = driver.findElement(By.xpath("//*[@id=\"summary_products_quantity\"]")).getText();
-		String pattern = "1 Product";	
-		Assert.assertEquals(extracted,pattern);
-		
-
-		  
-	}
-	
-	
-//	@Test(priority=4)
+//	@Test(priority=1)
+//	public void is_Sign_Out_Working() {
+//		if(driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[2]/a")).isDisplayed()) {
+//		  driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[2]/a")).click();
+//		  }
+//		Boolean flag = driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a")).isDisplayed();
+//		Assert.assertTrue(flag);
+//		  
+//	}  
+//	
+//	
+//	@Test(priority=2)
 //	public void click_On_Personal_Info() {
 //		
 //		  driver.findElement(By.xpath("//*[@id=\"center_column\"]/div/div[1]/ul/li[4]/a/span")).click();
@@ -94,6 +60,61 @@ public class HomePageTest {
 //		  }
 //		  
 //	}
+//	
+//	
+//	
+//	@Test(priority=3)
+//	public void Add_to_cart() {
+//		
+//		  driver.findElement(By.xpath("//*[@id=\"block_top_menu\"]/ul/li[2]/a")).click();
+//		WebElement e_to_hover_over=  driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[1]/div/a[1]/img"));
+//		Hover(driver, e_to_hover_over);
+//		driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/div[2]/a[1]/span")).click();
+//		
+//		driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/a/span")).click();
+//		
+//		
+//		String extracted = driver.findElement(By.xpath("//*[@id=\"summary_products_quantity\"]")).getText();
+//		String pattern = "1 Product";	
+//		Assert.assertEquals(extracted,pattern);
+//		
+//
+//		  
+//	}
+	
+	
+	@Test(priority=1)
+	public void Make_a_purchase() {
+		
+		  driver.findElement(By.xpath("//*[@id=\"block_top_menu\"]/ul/li[2]/a")).click();
+		WebElement e_to_hover_over=  driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[1]/div/a[1]/img"));
+		Hover(driver, e_to_hover_over);
+		driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/div[2]/a[1]/span")).click();
+		
+		driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/a/span")).click();
+		
+		
+		 driver.findElement(By.xpath("//*[@id=\"center_column\"]/p[2]/a[1]/span")).click();
+		 driver.findElement(By.xpath("//*[@id=\"center_column\"]/form/p/button/span")).click();
+		 driver.findElement(By.xpath("//*[@id=\"form\"]/p/button/span")).click();
+		 
+		 driver.findElement(By.xpath("//*[@id=\"cgv\"]")).click();
+		 
+		 if(driver.findElement(By.xpath("//*[@id=\"order\"]/div[2]/div/div")).isDisplayed()) {
+			 driver.findElement(By.xpath("//*[@id=\"order\"]/div[2]/div/div/a")).click();
+			 driver.findElement(By.xpath("//*[@id=\"form\"]/p/button/span")).click();
+		 }else {
+			 driver.findElement(By.xpath("//*[@id=\"form\"]/p/button/span")).click();
+		 }
+		 driver.findElement(By.xpath("//*[@id=\"HOOK_PAYMENT\"]/div[1]/div/p/a")).click();
+		 driver.findElement(By.xpath("//*[@id=\"cart_navigation\"]/button/span")).click();
+		 String extractedText = driver.findElement(By.xpath("//*[@id=\"center_column\"]/div/p/strong")).getText();
+		 String pattern = "Your order on My Store is complete.";
+		 Assert.assertEquals(extractedText, pattern);
+				 
+
+		  
+	}
 
 	
 	
