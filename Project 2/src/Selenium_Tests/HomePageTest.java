8a79da3dde0909eb1509c0bede46d8fdacf4ed20
Waheed Uddin Ahmed <waheed.ahmed@northsouth.edu>
2019-08-30 19:42:10 +0600

    fixed a problem of overlay in make_a_purchase function

--- Project 2/src/Selenium_Tests/HomePageTest.java
+++ Project 2/src/Selenium_Tests/HomePageTest.java
@@ -39,108 +39,116 @@ public class HomePageTest {
 		
 	}
 	
-//	@Test(priority=1)
-//	public void is_Sign_Out_Working() {
-//		if(driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[2]/a")).isDisplayed()) {
-//	  driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[2]/a")).click();
-//		  }
-//		Boolean flag = driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a")).isDisplayed();
-//		Assert.assertTrue(flag);
-//		  
-//	}  
-//	
-//	
-//	@Test(priority=2)
-//	public void click_On_Personal_Info() {
-//		
-//		  driver.findElement(By.xpath("//*[@id=\"center_column\"]/div/div[1]/ul/li[4]/a/span")).click();
-//	String Text= "YOUR PERSONAL INFORMATION";
-//		  if(driver.findElement(By.xpath("//*[@id=\"center_column\"]/div/h1")).isDisplayed()) {
-//			  String SegmentTitle = driver.findElement(By.xpath("//*[@id=\"center_column\"]/div/h1")).getText();
-//			  Assert.assertEquals(SegmentTitle,Text);
-//		  }
-//		  
-//}
-//	
-//	
-//	
-//@Test(priority=3)
-//	public void Add_to_cart() {
-//		
-//		  driver.findElement(By.xpath("//*[@id=\"block_top_menu\"]/ul/li[2]/a")).click();
-//		WebElement e_to_hover_over=  driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[1]/div/a[1]/img"));
-//		Hover(driver, e_to_hover_over);
-//		driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/div[2]/a[1]/span")).click();
-//		
-//		driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/a/span")).click();
-//		
-//		
-//		String extracted = driver.findElement(By.xpath("//*[@id=\"summary_products_quantity\"]")).getText();
-//		String pattern = "1 Product";	
-//		Assert.assertEquals(extracted,pattern);
-//		
-//
-//		  
-//	}
+	@Test(priority=1)
+	public void is_Sign_Out_Working() {
+		if(driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[2]/a")).isDisplayed()) {
+	  driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[2]/a")).click();
+		  }
+		Boolean flag = driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a")).isDisplayed();
+		Assert.assertTrue(flag);
+		  
+	}  
 	
-//	
-//	@Test(priority=1)
-//	public void Make_a_purchase() {
-//		
-//		  driver.findElement(By.xpath("//*[@id=\"block_top_menu\"]/ul/li[2]/a")).click();
-//		WebElement e_to_hover_over=  driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[1]/div/a[1]/img"));
-//		Hover(driver, e_to_hover_over);
-//		driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/div[2]/a[1]/span")).click();
-//		
-//		driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/a/span")).click();
-//		
-//		
-//		 driver.findElement(By.xpath("//*[@id=\"center_column\"]/p[2]/a[1]/span")).click();
-//		 driver.findElement(By.xpath("//*[@id=\"center_column\"]/form/p/button/span")).click();
-//		 driver.findElement(By.xpath("//*[@id=\"form\"]/p/button/span")).click();
-//		 WebElement el = driver.findElement(By.xpath("//*[@id=\"cgv\"]"));
-//		 
-//		  
-//		 
-//		 JavascriptExecutor executor = (JavascriptExecutor)driver;
-//		 executor.executeScript("arguments[0].click();", el);
-//		 
-//		
-//
-//			 driver.findElement(By.xpath("//*[@id=\"form\"]/p/button/span")).click();
-//
-//		 driver.findElement(By.xpath("//*[@id=\"HOOK_PAYMENT\"]/div[1]/div/p/a")).click();
-//		 driver.findElement(By.xpath("//*[@id=\"cart_navigation\"]/button/span")).click();
-//		 String extractedText = driver.findElement(By.xpath("//*[@id=\"center_column\"]/div/p/strong")).getText();
-//		 String pattern = "Your order on My Store is complete.";
-//		 Assert.assertEquals(extractedText, pattern);
-//				 
-//
-//		  
-//	}
+	
+	@Test(priority=2)
+	public void click_On_Personal_Info() {
+		
+		  driver.findElement(By.xpath("//*[@id=\"center_column\"]/div/div[1]/ul/li[4]/a/span")).click();
+	String Text= "YOUR PERSONAL INFORMATION";
+		  if(driver.findElement(By.xpath("//*[@id=\"center_column\"]/div/h1")).isDisplayed()) {
+			  String SegmentTitle = driver.findElement(By.xpath("//*[@id=\"center_column\"]/div/h1")).getText();
+			  Assert.assertEquals(SegmentTitle,Text);
+		  }
+		  
+}
+	
+	
+	
+@Test(priority=3)
+	public void Add_to_cart() {
+		
+		  driver.findElement(By.xpath("//*[@id=\"block_top_menu\"]/ul/li[2]/a")).click();
+		WebElement e_to_hover_over=  driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[1]/div/a[1]/img"));
+		Hover(driver, e_to_hover_over);
+		driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/div[2]/a[1]/span")).click();
+		
+		driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/a/span")).click();
+		
+		
+		String extracted = driver.findElement(By.xpath("//*[@id=\"summary_products_quantity\"]")).getText();
+		String pattern = "1 Product";	
+		Assert.assertEquals(extracted,pattern);
+		
+
+		  
+	}
+
+		@Test(priority=4)
+		public void searchBarTest() {
+			
+			// Clicked on the Search Bar
+			driver.findElement(By.xpath("//*[@id=\"search_query_top\"]")).click();
+			
+			// Typed query item on the search Bar
+			driver.findElement(By.xpath("//*[@id=\"search_query_top\"]")).sendKeys("Printed Summer Dress");
+			
+			// Clicked on the search button
+			driver.findElement(By.xpath("//*[@id=\"searchbox\"]/button")).click();
+			
+			String extractedText = driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/h5")).getText();
+			
+			String pattern = "Printed Summer Dress";
+			
+			
+			
+			 Assert.assertEquals(extractedText, pattern);
+			
+		}
+	
+	
+		@Test(priority=5)
+		public void Make_a_purchase() {
+			
+			  driver.findElement(By.xpath("//*[@id=\"block_top_menu\"]/ul/li[2]/a")).click();
+			WebElement e_to_hover_over=  driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[1]/div/a[1]/img"));
+			Hover(driver, e_to_hover_over);
+			driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/div[2]/a[1]/span")).click();
+			
+			driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/a/span")).click();
+			
+			
+			 driver.findElement(By.xpath("//*[@id=\"center_column\"]/p[2]/a[1]/span")).click();
+			 driver.findElement(By.xpath("//*[@id=\"center_column\"]/form/p/button/span")).click();
+			 driver.findElement(By.xpath("//*[@id=\"form\"]/p/button/span")).click();
+			 if(driver.findElement(By.className("fancybox-overlay")).isDisplayed()) {
+				 System.out.println("the overlay is in effect!");
+			 }
+			 JavascriptExecutor js = (JavascriptExecutor) driver;
+			 WebElement element = driver.findElement(By.className("fancybox-overlay"));
+			 js.executeScript("arguments[0].setAttribute('style', 'display:none')", element);
+			 
+			
+			 
+			  driver.findElement(By.xpath("//*[@id=\"cgv\"]")).click();
+			  
+	
+			 
+			
+	
+				 driver.findElement(By.xpath("//*[@id=\"form\"]/p/button/span")).click();
+	
+			 driver.findElement(By.xpath("//*[@id=\"HOOK_PAYMENT\"]/div[1]/div/p/a")).click();
+			 driver.findElement(By.xpath("//*[@id=\"cart_navigation\"]/button/span")).click();
+			 String extractedText = driver.findElement(By.xpath("//*[@id=\"center_column\"]/div/p/strong")).getText();
+			 String pattern = "Your order on My Store is complete.";
+			 Assert.assertEquals(extractedText, pattern);
+					 
+	
+			  
+		}
 
 	
-	@Test(priority=1)
-	public void searchBarTest() {
-		
-		// Clicked on the Search Bar
-		driver.findElement(By.xpath("//*[@id=\"search_query_top\"]")).click();
-		
-		// Typed query item on the search Bar
-		driver.findElement(By.xpath("//*[@id=\"search_query_top\"]")).sendKeys("Printed Summer Dress");
-		
-		// Clicked on the search button
-		driver.findElement(By.xpath("//*[@id=\"searchbox\"]/button")).click();
-		
-		String extractedText = driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/h5")).getText();
-		
-		String pattern = "Printed Summer Dress";
-		
-		
-		
-		 Assert.assertEquals(extractedText, pattern);
-		
-	}
+
 	
 	
 	
